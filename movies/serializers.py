c607dec27967d8f0dc390e7a179f7ce22e80a36d
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2022-04-27 13:56:14 +0700

    add serailizer api

---
+++ movies/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import field, fields
+from rest_framework import serializers
+from .models import Moviedata 
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Moviedata
+        fields = ['id', 'name', 'duration', 'rating']
+        
